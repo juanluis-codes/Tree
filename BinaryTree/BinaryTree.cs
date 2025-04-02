873e04e4e62a999e4a4fa50cc8c42f386024317e
Juan Luis López Delgado. <juanluistenista@gmail.com>
2025-04-02 16:01:05 +0200

    BinaryTree Traversing

--- BinaryTree/BinaryTree.cs
+++ BinaryTree/BinaryTree.cs
@@ -22,5 +22,37 @@
 
             node.Father = parent;
         }
+
+        public void Traverse(Node node)
+        {
+            Console.WriteLine(node.Value);
+            if (node.Left != null)
+            {
+                Traverse(node.Left);
+            }
+
+            if (node.Right != null)
+            {
+                Traverse(node.Right);
+            }
+        }
+
+        public void TraverseBreadthFirst(Node node)
+        {
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(node);
+            while (queue.Count > 0)
+            {
+                Node current = queue.Dequeue();
+                Console.WriteLine(current.Value);
+                if (current.Left != null)
+                {
+                    queue.Enqueue(current.Left);
+                }
+                if (current.Right != null) {
+                    queue.Enqueue(current.Right);
+                }
+            }
+        }
     }
 }
--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,16 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Tree;
+using BinaryTree;
 
-Node root = new Node(2, null);
-Node child1 = new Node(3, root);
-Node child2 = new Node(1, root);
-Node child3 = new Node(5, child1);
-Node child4 = new Node(6, child2);
-Node child5 = new Node(8, child2);
-Node child6 = new Node(9, child2);
+Console.WriteLine("Tree");
 
-Tree.Tree tree = new Tree.Tree(root);
-tree.Traverse(root);
+Tree.Node treeRoot = new Tree.Node(2, null);
+Tree.Node treeChild1 = new Tree.Node(3, treeRoot);
+Tree.Node treeChild2 = new Tree.Node(1, treeRoot);
+Tree.Node treeChild3 = new Tree.Node(5, treeChild1);
+Tree.Node treeChild4 = new Tree.Node(6, treeChild2);
+Tree.Node treeChild5 = new Tree.Node(8, treeChild2);
+Tree.Node treeChild6 = new Tree.Node(9, treeChild2);
+
+Tree.Tree tree = new Tree.Tree(treeRoot);
+tree.Traverse(treeRoot);
 Console.WriteLine();
-tree.TraverseBreadthFirst(root);
+tree.TraverseBreadthFirst(treeRoot);
+
+Console.WriteLine("Binary Tree");
+
+BinaryTree.Node binaryRoot = new BinaryTree.Node(2);
+BinaryTree.Node binaryChild1 = new BinaryTree.Node(1);
+BinaryTree.Node binaryChild2 = new BinaryTree.Node(4);
+BinaryTree.Node binaryChild3 = new BinaryTree.Node(8);
+BinaryTree.Node binaryChild4 = new BinaryTree.Node(3);
+BinaryTree.Node binaryChild5 = new BinaryTree.Node(6);
+BinaryTree.Node binaryChild6 = new BinaryTree.Node(5);
+BinaryTree.Node binaryChild7 = new BinaryTree.Node(7);
+
+BinaryTree.BinaryTree binaryTree = new BinaryTree.BinaryTree(binaryRoot);
+binaryTree.AddNode(binaryChild1, binaryRoot, true);
+binaryTree.AddNode(binaryChild2, binaryChild1, true);
+binaryTree.AddNode(binaryChild3, binaryChild1, false);
+binaryTree.AddNode(binaryChild4, binaryRoot, false);
+binaryTree.AddNode(binaryChild5, binaryChild4, true);
+binaryTree.AddNode(binaryChild6, binaryChild4, false);
+binaryTree.AddNode(binaryChild7, binaryChild6, false);
+
+binaryTree.Traverse(binaryRoot);
+Console.WriteLine();
+binaryTree.TraverseBreadthFirst(binaryRoot);
+
